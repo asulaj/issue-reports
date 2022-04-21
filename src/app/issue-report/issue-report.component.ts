f08dbe1c6350b18c1aca0bc0f989a6716e0bf6db
Albrim Sulaj <asulaj99@gmail.com>
2022-04-21 09:04:28 +0200

    added resolve feature/fix layout/all good

---
+++ src/app/issue-report/issue-report.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
+import {IssuesService} from '../issues.service';
+import {Output,EventEmitter} from '@angular/core';
+@Component({
+  selector: 'app-issue-report',
+  templateUrl: './issue-report.component.html',
+  styleUrls: ['./issue-report.component.css']
+})
+export class IssueReportComponent implements OnInit {
+  @Output() formClose = new EventEmitter();
+  issueForm: FormGroup | undefined;
+
+  constructor(private builder: FormBuilder, private issueService:IssuesService) { }
+
+  ngOnInit(): void {
+    this.issueForm = this.builder.group({
+      title: ['',Validators.required],
+      description: ['',Validators.required],
+      priority: ['',Validators.required],
+      type:['',Validators.required]
+    });
+  }
+  addIssue(){
+    if(this.issueForm && this.issueForm.invalid){
+      this.issueForm.markAllAsTouched();
+      return;
+    }
+    this.issueService.createIssue(this.issueForm?.value);
+    this.formClose.emit();
+  }
+
+}
