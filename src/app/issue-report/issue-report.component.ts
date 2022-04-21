b0dd42bd0f9f6ef8c907df6528936ae9740ca4aa
Albrim Sulaj <asulaj99@gmail.com>
2022-04-21 09:32:14 +0200

    +feature that checks for similar reports

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,33 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder,FormGroup,Validators} from '@angular/forms';
-import {IssuesService} from '../issues.service';
-import {Output,EventEmitter} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IssuesService } from '../issues.service';
+import { Output, EventEmitter } from '@angular/core';
+import { Issue } from '../issue';
 @Component({
   selector: 'app-issue-report',
   templateUrl: './issue-report.component.html',
-  styleUrls: ['./issue-report.component.css']
+  styleUrls: ['./issue-report.component.css'],
 })
 export class IssueReportComponent implements OnInit {
   @Output() formClose = new EventEmitter();
   issueForm: FormGroup | undefined;
+  suggestions: Issue[] = [];
 
-  constructor(private builder: FormBuilder, private issueService:IssuesService) { }
+  constructor(
+    private builder: FormBuilder,
+    private issueService: IssuesService
+  ) {}
 
   ngOnInit(): void {
     this.issueForm = this.builder.group({
-      title: ['',Validators.required],
-      description: ['',Validators.required],
-      priority: ['',Validators.required],
-      type:['',Validators.required]
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      priority: ['', Validators.required],
+      type: ['', Validators.required],
+    });
+    this.issueForm.controls.title.valueChanges.subscribe((title: string) => {
+      this.suggestions = this.issueService.getSuggestions(title);
     });
   }
-  addIssue(){
-    if(this.issueForm && this.issueForm.invalid){
+  addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
       this.issueForm.markAllAsTouched();
       return;
     }
     this.issueService.createIssue(this.issueForm?.value);
     this.formClose.emit();
   }
-
 }
